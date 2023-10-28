13a93cad5db89635810aeeaefcc4c29adf66a8e0
alireza <alirezaarzehgar82@gmail.com>
2023-10-28 16:49:00 +0330

    prototype routes
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -11,11 +11,29 @@ func todo(c echo.Context) error { return nil }
 
 func Init() *echo.Echo {
 	e := echo.New()
-	e.POST("/register", todo)
-	e.POST("/login", todo)
-	e.POST("/token/create", todo)
+	e.POST("/user/register", todo)
+	e.POST("/user/login", todo)
+
+	e.POST("/token/check", todo)
+	e.POST("/guest/create-token", todo)
 
 	g := e.Group("", echojwt.WithConfig(echojwt.Config{SigningKey: config.JwtSecret()}))
+	g.GET("/user/story/count", todo)
+	g.DELETE("/user/delete-account", todo)
+
+	g.GET("/story/pub/:code", todo)
 	g.POST("/story/create", todo)
+	g.GET("/story/stories", todo)
+	g.DELETE("/story/:code", todo)
+	g.PUT("/story/:code", todo)
+	g.POST("/story/publish/:code", todo)
+	g.POST("/story/convert", todo)
+
+	g.GET("/guest/settings", todo)
+	g.PUT("/guest/settings", todo)
+	g.DELETE("/guest/delete-account", todo)
+	g.POST("/guest/save-story", todo)
+	g.GET("/guest/stories", todo)
+
 	return e
 }
