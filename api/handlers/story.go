b9e33b3f7aec82beca8744f7a9bdd1a61f19271d
alireza <alirezaarzehgar82@gmail.com>
2023-11-03 19:31:45 +0330

    remove filtering specific texts on GetStoryInfo()
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- api/handlers/story.go
+++ api/handlers/story.go
@@ -207,22 +207,7 @@ func GetStoryInfo(c echo.Context) error {
 		})
 	}
 
-	return c.JSON(http.StatusOK, map[string]any{
-		"status": true,
-		"data": map[string]any{
-			"text":             story.Text,
-			"text_position":    story.TextPosition,
-			"text_size":        story.TextSize,
-			"is_shared":        story.IsShared,
-			"type":             story.Type,
-			"code":             story.Code,
-			"name":             story.Name,
-			"from":             story.From,
-			"to":               story.To,
-			"background":       story.BackgroundUrl,
-			"background_color": story.BackgroundColor,
-		},
-	})
+	return c.JSON(http.StatusOK, map[string]any{"status": true, "data": story})
 }
 
 func EditStoryInfo(c echo.Context) error {
