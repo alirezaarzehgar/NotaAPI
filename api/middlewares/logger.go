bece15213efd56cefd3c97e8d401b0b0ef18a457
alireza <alirezaarzehgar82@gmail.com>
2023-11-04 20:12:40 +0330

    ignore log routes for log middleware and don't show empty body
    Signed-off-by: alireza <alirezaarzehgar82@gmail.com>

--- api/middlewares/logger.go
+++ api/middlewares/logger.go
@@ -2,17 +2,29 @@ package middlewares
 
 import (
 	"encoding/json"
-
-	"github.com/labstack/echo/v4"
+	"strings"
 
 	"github.com/Asrez/NotaAPI/utils"
+	"github.com/labstack/echo/v4"
 )
 
 func LogRequests(next echo.HandlerFunc) echo.HandlerFunc {
 	return echo.HandlerFunc(func(c echo.Context) error {
+		if strings.HasPrefix(c.Request().URL.Path, "/logs") {
+			return next(c)
+		}
+
 		var data map[string]any
+
+		var output []any
+		output = append(output, c.Request().Method, c.Request().URL.Path)
+
+		if data != nil {
+			output = append(output, ":", data)
+		}
+
 		json.NewDecoder(c.Request().Body).Decode(&data)
-		utils.DebugLog(c.Request().URL.Path, ":", data)
+		utils.DebugLog(output...)
 		return next(c)
 	})
 }
